f5ec96fa16d9eee9ef1373c0b34a10786b668237
ss <dark989826@gmail.com>
2022-01-17 21:22:49 +0300

    fixed errors. Stable on Ubuntu 20

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
+#!/bin/sh
 sudo apt-get install -y python3-pip
-sudo useradd -p d62f5c72eec752e8b4f923ef proxy
-sudo su -u proxy -c python3 -m pip install proxy.py
+sudo useradd -m -p d62f5c72eec752e8b4f923ef pproxy
+sudo su pproxy -c "python3 -m pip install proxy.py"
 sudo cp ./proxy.service /etc/systemd/system/
 sudo systemctl enable proxy
 sudo systemctl start proxy
